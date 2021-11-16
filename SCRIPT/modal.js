868024b83e4b12aeb7654a0f230504529b280959
natsumi-nassan <92068277+natsumi-nassan@users.noreply.github.com>
2021-11-16 18:29:56 +0900

    モーダル実装

---
+++ SCRIPT/modal.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded',function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
