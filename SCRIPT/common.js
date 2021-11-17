315344922691057580e26457c206551b5f8db66b
natsumi-nassan <92068277+natsumi-nassan@users.noreply.github.com>
2021-11-17 17:23:55 +0900

    スマホ用ヘッダー整え

--- SCRIPT/common.js
+++ SCRIPT/common.js
@@ -24,3 +24,14 @@ window.addEventListener('DOMContentLoaded',function(){
         $('header').removeClass('add-opacity');
     }
 });
+
+//右寄りハンバーガーメニュー実装
+function hamburger() {
+  document.getElementById('line1').classList.toggle('line_1');
+  document.getElementById('line2').classList.toggle('line_2');
+  document.getElementById('line3').classList.toggle('line_3');
+  document.getElementById('nav').classList.toggle('in');
+}
+document.getElementById('hamburger').addEventListener('click' , function () {
+  hamburger();
+} );
