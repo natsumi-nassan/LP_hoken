39910bf1b04f5c8636b96a6dff43663a377c6239
natsumi-nassan <92068277+natsumi-nassan@users.noreply.github.com>
2021-11-25 11:02:48 +0900

    モーダル展開時スクロール不可制御

--- SCRIPT/modal.js
+++ SCRIPT/modal.js
@@ -1,10 +1,15 @@
+//モーダル開閉時のアニメーション定義
 $(function(){
-    $('.js-modal-open').on('click',function(){
-        $('.js-modal').fadeIn();
+    $('.modal-open').on('click',function(){
+        $('.modal').fadeIn();
+        $('body').css('overflow-y','hidden');  // 本文の縦スクロールを無効
+        $('body').css('padding-right','16px'); // スクロールバー分の余白を付与
         return false;
     });
-    $('.js-modal-close').on('click',function(){
-        $('.js-modal').fadeOut();
+    $('.modal-close').on('click',function(){
+        $('.modal').fadeOut();
+        $('body').css('overflow-y','auto');     // 本文の縦スクロールを有効
+        $('body').css('padding-right','0px');
         return false;
     });
 });
